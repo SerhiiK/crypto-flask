26393bf060f205d80b4bad407121bbfc21fe0265
Serhii Krupskyi <6170876+SerhiiK@users.noreply.github.com>
2022-07-10 16:08:34 +0300

    Add mock for about page

--- crypto_flask/main.py
+++ crypto_flask/main.py
@@ -3,5 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def main():
     return "<p>Hello</p>"
+
+@app.route("/about")
+def about_page():
+    return "<p> Will be filled soon....</p>"
